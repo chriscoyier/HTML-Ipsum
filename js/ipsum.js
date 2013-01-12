da0bafe362c24ea905ffaa62a11eeaa16e5a78eb
geoff.thibeau <geoff@propelgroup.com>
2013-01-11 18:17:55 -0600

    Changed ZeroClipboard to click event
    This plugin wasn't working for me at all, so I don't really know what
    it's supposed to do. Since it was breaking the responsive css (by
    hardcoding absolute positioned DIVs), I changed it to a click event
    instead of an each.
    
    If this breaks stuff, disregard my change. I simply wasn't seeing it
    display at all.

--- js/ipsum.js
+++ js/ipsum.js
@@ -1,15 +1,14 @@
-$("legend").each(function() {
+$("legend").click(function() { 
 
     var clip = new ZeroClipboard.Client();
     var thisObj = $(this);
     clip.glue(thisObj[0]);
     var txt = $(this).parent().find("textarea").text();
     clip.setText(txt);
-    
+
     clip.addEventListener('complete', function(client, text) {
         $("#copied-notice").fadeIn(500, function(){
-          $(this).fadeOut(500);  
+          $(this).fadeOut(500);
         });
-    });        
-
+    });
 }); 
